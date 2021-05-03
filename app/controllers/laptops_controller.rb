02395179191be599019a13a8e32f0fdb39830f99
Clothilde Maurer <clothilde.maurer@gmail.com>
2021-05-03 16:11:06 +0200

    Add laptops index

--- app/controllers/laptops_controller.rb
+++ app/controllers/laptops_controller.rb
@@ -1,4 +1,10 @@
 class LaptopsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @laptops = Laptop.all
+  end
+
   def show
     @laptop = Laptop.find(params[:id])
   end
