6b4e592f9238e868338a353393d1a953605cf42a
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-05-04 12:11:06 +0200

    Add booked laptops, no style

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,6 @@ class DashboardsController < ApplicationController
   def show
     @laptops = Laptop.where(user: current_user)
     @bookings = Booking.where(user: current_user)
+    @booked = Booking.where(laptop: current_user.laptops)
   end
 end
