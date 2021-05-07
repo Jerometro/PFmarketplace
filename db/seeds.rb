aa89f4bad1fa9de16f1f11cedf94968a61e1567e
Clothilde Maurer <clothilde.maurer@gmail.com>
2021-05-07 12:15:41 +0200

    improved seeds and forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ require 'open-uri'
 file1 = URI.open("https://images.pexels.com/photos/69432/pexels-photo-69432.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
 file2 = URI.open("https://images.pexels.com/photos/4348404/pexels-photo-4348404.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
 file3 = URI.open("https://images.pexels.com/photos/7091926/pexels-photo-7091926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-file4 = URI.open("https://images.pexels.com/photos/441963/pexels-photo-441963.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+file4 = URI.open("https://images.pexels.com/photos/4348404/pexels-photo-4348404.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
 file5 = URI.open("https://images.pexels.com/photos/6696871/pexels-photo-6696871.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
 file6 = URI.open("https://images.pexels.com/photos/6696863/pexels-photo-6696863.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
 file7 = URI.open("https://images.pexels.com/photos/434346/pexels-photo-434346.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
@@ -53,8 +53,8 @@ marine = User.create!({
 })
 
 mac_de_serge = Laptop.new({
-  name: 'Macbook Pro - 13 pouces',
-  description: 'Very good machine. Grey. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Apple MacBook Pro 13 pouces Retina',
+  description: 'Very good machine. Grey. Core i7 3,1 GHz - SSD 512 Go - 16 Go AZERTY - Français',
   address: '17 avenue de la République 35000 Rennes',
   price_per_day: 150,
   user: serge
@@ -63,8 +63,8 @@ mac_de_serge.photo.attach(io: file1, filename: 'mac.jpeg', content_type: 'images
 mac_de_serge.save!
 
 lenovo_de_serge = Laptop.new({
-  name: 'Lenovo - 15 pouces',
-  description: 'Super fast laptop. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Lenovo - 15,6 pouces',
+  description: 'Super fast laptop. Processeur Intel® Core™ i5-1035G1 10e génération',
   address: '17 avenue de la République 35000 Rennes',
   price_per_day: 150,
   user: serge
@@ -73,8 +73,8 @@ lenovo_de_serge.photo.attach(io: file2, filename: 'leno.jpeg', content_type: 'im
 lenovo_de_serge.save!
 
 msi_de_serge = Laptop.new({
-  name: 'MSI - 15 pouces',
-  description: 'Ideal for gaming. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'MSI - 15,6 pouces',
+  description: 'Ideal for gaming. Intel Core i7-9750H 12 Mo 2.6 GHz',
   address: '17 avenue de la République 35000 Rennes',
   price_per_day: 150,
   user: serge
@@ -84,7 +84,7 @@ msi_de_serge.save!
 
 lenovo_de_paul = Laptop.new({
   name: 'Lenovo - 13 pouces',
-  description: 'Black color, a bit used. Intel Core i7 16 Go RAM 512 Go SSD',
+  description: 'Black color, a bit used. Processeur Intel® Core™ i5-1035G1 10e génération',
   address: '30 avenue Charles de Gaulle 35000 Rennes',
   price_per_day: 50,
   user: paul
@@ -93,8 +93,8 @@ lenovo_de_paul.photo.attach(io: file4, filename: 'lenovo.jpeg', content_type: 'i
 lenovo_de_paul.save!
 
 mac_de_paul = Laptop.new({
-  name: 'MacBook Air - 13 pouces',
-  description: 'Spacegrey, brand new. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Apple MacBook Air - 13 pouces',
+  description: 'Spacegrey, brand new. Core i7 3,1 GHz - SSD 512 Go - 16 Go AZERTY - Français',
   address: '30 avenue Charles de Gaulle 35000 Rennes',
   price_per_day: 100,
   user: paul
@@ -103,8 +103,8 @@ mac_de_paul.photo.attach(io: file9, filename: 'mac1.jpeg', content_type: 'images
 mac_de_paul.save!
 
 msi_de_paul = Laptop.new({
-  name: 'MSI - 18 pouces',
-  description: 'Ideal for designers. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'MSI - 17,3 pouces',
+  description: 'Ideal for designers. Intel Core i7-9750H 12 Mo 2.6 GHz',
   address: '17 avenue de la République 35000 Rennes',
   price_per_day: 70,
   user: paul
@@ -113,8 +113,8 @@ msi_de_paul.photo.attach(io: file11, filename: 'msi1.jpeg', content_type: 'image
 msi_de_paul.save!
 
 mac_de_marine = Laptop.new({
-  name: 'MacBook Pro - 15 pouces',
-  description: 'Ideal for web development. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Apple MacBook Pro - 15 pouces',
+  description: 'Ideal for web development. Core i7 3,1 GHz - SSD 512 Go - 16 Go AZERTY - Français',
   address: '24 rue du Bois 35000 Rennes',
   price_per_day: 100,
   user: marine
@@ -123,8 +123,8 @@ mac_de_marine.photo.attach(io: file10, filename: 'mac2.jpeg', content_type: 'ima
 mac_de_marine.save!
 
 asus_de_marine = Laptop.new({
-  name: 'Asus',
-  description: 'PC Ultra-Portable. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Asus - 15.6 pouces',
+  description: 'PC Ultra-Portable. Intel i5-1035G1 - 8 Go RAM - 256 Go SSD - Windows 10',
   address: '24 rue du Bois 35000 Rennes',
   price_per_day: 30,
   user: marine
@@ -133,8 +133,8 @@ asus_de_marine.photo.attach(io: file5, filename: 'asus.jpeg', content_type: 'ima
 asus_de_marine.save!
 
 acer_de_marine = Laptop.new({
-  name: 'Acer',
-  description: 'A bit old but very handy. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Acer Swift 3 - 13,5 pouces',
+  description: 'A bit old but very handy. Full HD (1920 x 1080) 16:9 IPS 16 Go, LPDDR4X 1 To SSD',
   address: '24 rue du Bois 35000 Rennes',
   price_per_day: 20,
   user: marine
@@ -143,8 +143,8 @@ acer_de_marine.photo.attach(io: file6, filename: 'acer.jpeg', content_type: 'ima
 acer_de_marine.save!
 
 mac_de_bertrand = Laptop.new({
-  name: 'MacBook Air - 15 pouces',
-  description: 'Light and easy to carry. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Apple MacBook Air - 15 pouces',
+  description: 'Light and easy to carry. Core i7 3,1 GHz - SSD 512 Go - 16 Go AZERTY - Français',
   address: '30 rue de La Plaine 35000 Rennes',
   price_per_day: 80,
   user: bertrand
@@ -153,8 +153,8 @@ mac_de_bertrand.photo.attach(io: file7, filename: 'mac5.jpeg', content_type: 'im
 mac_de_bertrand.save!
 
 acer_de_bertrand = Laptop.new({
-  name: 'Acer Aspire',
-  description: 'Recommended to work. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Acer Aspire - 15,6 pouces',
+  description: 'Recommended to work. Processeur Intel® Core™ i3-1005G1 (1,2 GHz / 3,4 GHz)',
   address: '30 rue de La Plaine 35000 Rennes',
   price_per_day: 50,
   user: bertrand
@@ -163,8 +163,8 @@ acer_de_bertrand.photo.attach(io: file8, filename: 'acer1.jpeg', content_type: '
 acer_de_bertrand.save!
 
 huawei_de_bertrand = Laptop.new({
-  name: 'Huawei',
-  description: 'Touch-screen. Intel Core i7 16 Go RAM 512 Go SSD',
+  name: 'Huawei Matebook X Pro - 23,8 pouces',
+  description: 'Intel® Core™ i5-1135G7 - Écran FullView 3K / Tactile',
   address: '30 rue de La Plaine 35000 Rennes',
   price_per_day: 110,
   user: bertrand
